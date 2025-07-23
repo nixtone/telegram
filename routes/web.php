a512b1baa7b4ac4ceb6f525eb210923e782f2380
nixtone <nixtone@nxt.ru>
2025-07-23 08:21:57 +0300

    send msg

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use DefStudio\Telegraph\Facades\Telegraph;
+use DefStudio\Telegraph\Models\TelegraphChat;
 
 Route::get('/', function () {
+    $chat = TelegraphChat::all();
+    $chat->html("<strong>Hello!</strong>\n\nI'm here!")->send();
     $send = Telegraph::message('this is great')->send();
     dd($send);
 
